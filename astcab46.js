7d6f3ad147dc718321687cd0dd0f6ab2b003c715
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 14:55:29 +0100

    Update astcab46.js

--- astcab46.js
+++ astcab46.js
@@ -9,8 +9,7 @@ function sbrTgl(){
     sbr.classList.replace('sidebar-invisible','sidebar-visible');
     body.classList.add('sidebar-visible');
 }
-}
-if (sbrIsOpn){
+ if (sbrIsOpn){
    window.onclick = function(e){
       if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
       sbr.classList.replace('sidebar-visible','sidebar-invisible');
@@ -18,6 +17,9 @@ if (sbrIsOpn){
     } 
   }                
  }
+    
+}
+
 sbrBtn.addEventListener('click', sbrTgl, false );
 
 
