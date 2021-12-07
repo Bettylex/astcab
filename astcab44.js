6933b98e819be90ea4d58d110dc5f23dad62a450
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 14:51:11 +0100

    Update astcab44.js

--- astcab44.js
+++ astcab44.js
@@ -1,23 +1,22 @@
 var body = document.body,
-      sbrBtn = document.querySelector(".hamburger-menu-container"),
-      sbr = document.getElementById("sidebar"),
+    sbrBtn = document.querySelector(".hamburger-menu-container"),
+    sbr = document.getElementById("sidebar"),
     sbrIsOpn = sbr.matches('.sidebar-visible');
 
 // Sidebar Toggle
 function sbrTgl(){
-  if (!sbrIsOpn){   
-    sbr.classList.replace('sidebar-invisible','sidebar-visible');
-    body.classList.add('sidebar-visible');
-}
-}   
-if (sbrIsOpn){  
+  if (sbrIsOpn){
    window.onclick = function(e){
       if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
       sbr.classList.replace('sidebar-visible','sidebar-invisible');
-      body.classList.remove('sidebar-visible');     
+      body.classList.remove('sidebar-visible');
+    } 
   }                
- } 
-
+ } else {   
+    sbr.classList.replace('sidebar-invisible','sidebar-visible');
+    body.classList.add('sidebar-visible');
+}
+}
 sbrBtn.addEventListener('click', sbrTgl, false );
 
 
