945d5e9c456fa51cf7123016dd5c83f0533031ef
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 09:43:29 +0100

    Update and rename astcab1.js to astcab2.js

--- astcab2.js
+++ astcab2.js
@@ -1,8 +1,11 @@
+const body = document.body;
+
 // Sidebar Toggle
 function sbrTgl(){
     const sbr = document.querySelector(".sidebar-container");
-    sbr.classList.toggle('sidebar-invisible');
-    sbr.classList.toggle('sidebar-visible');
+    sbr.classList.remove('sidebar-invisible');
+    sbr.classList.add('sidebar-visible');
+    body.classList.toggle('sidebar-visible');
 }
 const sbrBtn = document.querySelector(".hamburger-menu-container");
 sbrBtn.addEventListener('click', sbrTgl );
