896fc4b1c8d4c1bfec63571c51a316a7f02fb1e6
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 09:30:20 +0100

    Delete astcab.js

--- astcab.js
+++
@@ -1,7 +0,0 @@
-// Sidebar Toggle
-function sbrTgl(){
-    this.classList.toggle('sidebar-invisible');
-    this.classList.toggle('sidebar-visible');
-}
-const sbrBtn = document.querySelector(".hamburger-menu-container");
-sbrBtn.addEventListener('click', sbrTgl );
