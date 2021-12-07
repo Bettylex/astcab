8c5b3953c73e5c147b9bddb36aa22828d292ca37
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 15:00:08 +0100

    Update astcab47.js

--- astcab47.js
+++ astcab47.js
@@ -1,25 +1,23 @@
 var body = document.body,
-    sbrBtn = document.querySelector(".hamburger-menu-container"),
-    sbr = document.getElementById("sidebar"),
-    sbrIsOpn = sbr.matches('.sidebar-visible');
+  sbrBtn = document.querySelector(".hamburger-menu-container"),
+  sbr = document.getElementById("sidebar"),
+  sbrIsOpn = sbr.matches('.sidebar-visible');
 
 // Sidebar Toggle
-function sbrTgl(){
- if (!sbrIsOpn){   
-    sbr.classList.replace('sidebar-invisible','sidebar-visible');
+function sbrTgl() {
+  if (!sbrIsOpn) {
+    sbr.classList.replace('sidebar-invisible', 'sidebar-visible');
     body.classList.add('sidebar-visible');
-}
- if (sbrIsOpn){
-   window.onclick = function(e){
-      if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
-      sbr.classList.replace('sidebar-visible','sidebar-invisible');
-      body.classList.remove('sidebar-visible');
-    } 
-  }                
- }
-    
+
+    window.addEventListener("click", function(e) {
+      if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay')) {
+        sbr.classList.replace('sidebar-visible', 'sidebar-invisible');
+        body.classList.remove('sidebar-visible');
+      }
+    });
+  }
+
 }
 
-sbrBtn.addEventListener('click', sbrTgl, false );
-
+sbrBtn.addEventListener('click', sbrTgl, false);
 
