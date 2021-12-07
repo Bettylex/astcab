1dd9c1949d4c2bc7258f6131dda8f354f43e53ea
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 10:52:28 +0100

    Update astcab7.js

--- astcab7.js
+++ astcab7.js
@@ -3,7 +3,7 @@ var body = document.body,
       sbr = document.querySelector(".sidebar-container");
 
 // Sidebar Toggle
-if (sbr.classList.contains('sidebar-invisible') ) {
+if(sbr.classList.contains('sidebar-invisible') ) {
 function sbrOpen(){  
     sbr.classList.remove('sidebar-invisible');
     sbr.classList.add('sidebar-visible');
@@ -12,7 +12,7 @@ function sbrOpen(){
 }
 sbrBtn.addEventListener('click', sbrOpen, false );
 
-if (sbr.classList.contains('sidebar-visible')) {
+if(sbr.classList.contains('sidebar-visible')) {
 function sbrClose(e) {
     if (e.target.matches('.bg-overlay') || e.target.matches('.touch-icon') ) {
       sbr.classList.remove('sidebar-visible');
