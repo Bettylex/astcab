368d5e4d30b96c03183484414aef818dd5e40db5
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 15:21:13 +0100

    Update astcab52.js

--- astcab52.js
+++ astcab52.js
@@ -4,20 +4,21 @@ var body = document.body,
   sbrIsOpn = sbr.matches('.sidebar-visible');
 
 // Sidebar Toggle
-function sbrTgl() {
+function sbrOpn() {
   if (!sbrIsOpn) {
     sbr.classList.replace('sidebar-invisible', 'sidebar-visible');
     body.classList.add('sidebar-visible');
   }
 }
-  if (sbrIsOpn) {
-  window.addEventListener("click", function(e) {
-        if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay')) {
-          sbr.classList.replace('sidebar-visible', 'sidebar-invisible');
-          body.classList.remove('sidebar-visible');
-        }
-     });
+
+function sbrCls() {
+  if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay')) {
+    sbr.classList.replace('sidebar-visible', 'sidebar-invisible');
+    body.classList.remove('sidebar-visible');
   }
+}
 
-sbrBtn.addEventListener('click', sbrTgl, false);
-
+sbrBtn.addEventListener('click', sbrOpn, false);
+if (sbrIsOpn) {
+  body.addEventListener('click', sbrCls, false);
+}
