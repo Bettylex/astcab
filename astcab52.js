b1088018ad01bf2b185f1e59eccc9b875f0be3a8
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 15:13:28 +0100

    Update astcab52.js

--- astcab52.js
+++ astcab52.js
@@ -1,7 +1,7 @@
 var body = document.body,
   sbrBtn = document.querySelector(".hamburger-menu-container"),
   sbr = document.getElementById("sidebar"),
-  sbrIsOpn = sbr.classList.contains('sidebar-visible');
+  sbrIsOpn = sbr.matches('.sidebar-visible');
 
 // Sidebar Toggle
 function sbrTgl() {
