d0f73a56ef73476ba2be2e55fb22520e8ec9092f
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 09:52:06 +0100

    Update astcab1.js

--- astcab1.js
+++ astcab1.js
@@ -1,11 +1,13 @@
-const body = document.body;
+const body = document.body,
+      sbrBtn = document.querySelector(".hamburger-menu-container"),
+      sbr = document.querySelector(".sidebar-container");
 
 // Sidebar Toggle
-function sbrTgl(){
-    const sbr = document.querySelector(".sidebar-container");
+function sbrOpn(){
+    if (sbr.classList.contains(sidebar-invisible))
     sbr.classList.remove('sidebar-invisible');
     sbr.classList.add('sidebar-visible');
     body.classList.toggle('sidebar-visible');
 }
-const sbrBtn = document.querySelector(".hamburger-menu-container");
+
 sbrBtn.addEventListener('click', sbrTgl );
