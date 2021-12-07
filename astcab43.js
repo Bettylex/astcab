6bafb0b525e01c0026424cc16aa877186e06c509
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 14:34:13 +0100

    Update and rename astcab42.js to astcab43.js

--- astcab43.js
+++ astcab43.js
@@ -1,6 +1,6 @@
 var body = document.body,
       sbrBtn = document.querySelector(".hamburger-menu-container"),
-      sbr = document.getElementById("#sidebar"),
+      sbr = document.getElementById("sidebar"),
     sbrIsOpn = sbr.matches('.sidebar-visible');
 
 // Sidebar Toggle
@@ -10,7 +10,7 @@ function sbrTgl(){
     body.classList.add('sidebar-visible');
 } else {      
 window.onclick = function(e){
-      if (e.target.matches('.navigation>.touch-icon') || e.target.matches('.bg-overlay') ) {
+      if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
       sbr.classList.replace('sidebar-visible','sidebar-invisible');
       body.classList.remove('sidebar-visible');     
   }                
