9b8abb0eff7eaf9f8a13c333abe090feefaa2c50
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 12:44:49 +0100

    Update astcab24.js

--- astcab24.js
+++ astcab24.js
@@ -3,15 +3,22 @@ var body = document.body,
       sbr = document.querySelector('.sidebar-container');
 
 // Sidebar Toggle
-function sbrOpen(){  
+function sbrOpen(){
+    if(sbr.classList.contains('sidebar-invisible')){
     sbr.classList.replace('sidebar-invisible', 'sidebar-visible');
+    }
+if(!body.classList.contains('sidebar-visible')){
     body.classList.add('sidebar-visible');
 }
 sbrBtn.addEventListener('click', sbrOpen, false );
 
 function sbrClose() {
+      if(sbr.classList.contains('sidebar-visible')){
       sbr.classList.replace('sidebar-visible', 'sidebar-invisible');
-      body.classList.remove('sidebar-visible');   
+      }  
+      if(!body.classList.contains('sidebar-visible')){
+      body.classList.remove('sidebar-visible');
+      }      
   }
 var sbrNav = document.querySelector('.navigation>.touch-icon'),
     sbrOver = document.querySelector('.sidebar-container+.bg-overlay');
