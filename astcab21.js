2b44f4dbf9e0741cd5057150a4f31c2630c8ed9d
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 11:58:22 +0100

    Update and rename astcab20.js to astcab21.js

--- astcab21.js
+++ astcab21.js
@@ -5,20 +5,19 @@ var body = document.body,
 
 // Sidebar Toggle
 function sbrOpen(){  
-    sbr.classList.remove('sidebar-invisible');
-    sbr.classList.add('sidebar-visible');
-    body.classList.add('sidebar-visible');          
+    sbr.classList.replace("sidebar-invisible", "sidebar-visible");
+    body.classList.add('sidebar-visible');
+      
 }
 sbrBtn.addEventListener('click', sbrOpen, false );
 
 function sbrClose() {
-      sbr.classList.remove('sidebar-visible');
-      body.classList.remove('sidebar-visible');
-      sbr.classList.add('sidebar-invisible');
+      sbr.classList.replace("sidebar-visible", "sidebar-invisible");
+      body.classList.remove('sidebar-visible');   
   }
 if(!sbrIsOpn){ 
 sbrBtn.addEventListener('click', sbrOpen, false );
 } else { 
-var sbrNav = document.querySelector('.navigation .touch-icon');
+var sbrNav = document.querySelector('.navigation>.touch-icon');
 sbrNav.addEventListener('click', sbrClose, false );
 }
