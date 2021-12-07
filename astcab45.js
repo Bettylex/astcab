51c9f83472ef2c6911c27ae3c852562c27caa2e4
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 14:53:35 +0100

    Update astcab45.js

--- astcab45.js
+++ astcab45.js
@@ -5,18 +5,19 @@ var body = document.body,
 
 // Sidebar Toggle
 function sbrTgl(){
-  if (sbrIsOpn){
+ if (!sbrIsOpn){   
+    sbr.classList.replace('sidebar-invisible','sidebar-visible');
+    body.classList.add('sidebar-visible');
+}
+}
+if (sbrIsOpn){
    window.onclick = function(e){
       if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
       sbr.classList.replace('sidebar-visible','sidebar-invisible');
       body.classList.remove('sidebar-visible');
     } 
   }                
- } else {   
-    sbr.classList.replace('sidebar-invisible','sidebar-visible');
-    body.classList.add('sidebar-visible');
-}
-}
+ }
 sbrBtn.addEventListener('click', sbrTgl, false );
 
 
