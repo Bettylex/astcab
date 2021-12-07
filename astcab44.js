486ea27b3d06793fcafba8b87b7008a49314123d
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 14:47:27 +0100

    Update and rename astcab43.js to astcab44.js

--- astcab44.js
+++ astcab44.js
@@ -8,15 +8,16 @@ function sbrTgl(){
   if (!sbrIsOpn){   
     sbr.classList.replace('sidebar-invisible','sidebar-visible');
     body.classList.add('sidebar-visible');
-} else {      
-window.onclick = function(e){
+}
+}   
+if (sbrIsOpn){  
+   window.onclick = function(e){
       if (e.target.matches('.touch-icon') || e.target.matches('.bg-overlay') ) {
       sbr.classList.replace('sidebar-visible','sidebar-invisible');
       body.classList.remove('sidebar-visible');     
   }                
- }    
-}           
-}
+ } 
+
 sbrBtn.addEventListener('click', sbrTgl, false );
 
 
