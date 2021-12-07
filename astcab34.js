bb3ec1fb20a05e1c6cfc149e494bd0129c716a81
Bettylex <86284652+Bettylex@users.noreply.github.com>
2021-12-07 13:58:54 +0100

    Update and rename astcab33.js to astcab34.js

---
+++ astcab34.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+var body = document.body,
+      sbrBtn = document.querySelector(".hamburger-menu-container"),
+      sbr = document.querySelector(".sidebar-container"),
+
+// Sidebar Toggle
+function sbrOpen(){  
+    sbr.classList.remove('sidebar-invisible');
+    sbr.classList.add('sidebar-visible');
+    body.classList.add('sidebar-visible');          
+}
+sbrBtn.addEventListener('click', sbrOpen, false );
+
+var sbrOver = document.querySelector(".bg-overlay"),
+sbrNav = document.querySelector(".touch-icon");
+function sbrClose() {
+      sbr.classList.remove('sidebar-visible');
+      body.classList.remove('sidebar-visible');
+      sbr.classList.add('sidebar-invisible');
+  }
+sbrOver.addEventListener('click', sbrClose, false );
+sbrNav.addEventListener('click', sbrClose, false );
+    
+}, false);  //DOMContentLoaded end
